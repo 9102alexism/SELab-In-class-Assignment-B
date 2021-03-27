64224465ec387d4f0f9a0fba95de06661aef0d75
fahim <fshakil163014@bscse.uiu.ac.bd>
2021-03-27 12:37:20 +0600

    5th commit

--- sum_forloop.c
+++ sum_forloop.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 
-<<<<<<< HEAD
-=======
 int sum(int arr[], int n) {
     int s = 0;
     int i;
@@ -12,28 +10,33 @@ int sum(int arr[], int n) {
 }
 
 
->>>>>>> fahim_test
 int main() {
-    int i,j,k,n=3;
-    int arr[] = {1, 3, 5};
-    printf("sum of the elements of this array is %d", sum(arr, n));
-    return 0;
-}
+    int arr[100];
+    int i,j,k,n;
 
-<<<<<<< HEAD
-int sum(int arr[], int n) {
-    int s = 0;
-    int i;
-    for (i=0;i<n;i++){
-        s += arr[i];
-=======
     printf("Enter size of array ");
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         printf("%dth element ",i+1);
         scanf("%d",&arr[i]);
->>>>>>> fahim_test
+    }
+
+    printf("sum of the elements of this array is %d", sum(arr, n));
+    return 0;
+}
+
+int sum(int arr[], int n) {
+    int s = 0;
+    int i;
+    for (i=0;i<n;i++){
+        s += arr[i];
+    printf("Enter size of array ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%dth element ",i+1);
+        scanf("%d",&arr[i]);
     }
     return s;
 }
